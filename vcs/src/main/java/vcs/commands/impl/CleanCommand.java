929c0a41e2c1313a87ca73aa2767712f98eb5b85
ilnurshug <ilnur.shugaepov@mail.ru>
2016-10-01 14:41:10 +0300

    clean command fix

--- vcs/src/main/java/vcs/commands/impl/CleanCommand.java
+++ vcs/src/main/java/vcs/commands/impl/CleanCommand.java
@@ -1,16 +1,9 @@
 package vcs.commands.impl;
 
-import com.beust.jcommander.Parameters;
 import vcs.commands.Command;
 import vcs.config.CommitConfig;
 import vcs.config.GlobalConfig;
 import vcs.util.VcsUtils;
-
-import java.io.File;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class CleanCommand extends Command {
     @Override
@@ -20,15 +13,6 @@ public class CleanCommand extends Command {
 
     @Override
     protected void execImpl() {
-        List<String> files = new LinkedList<>();
-        files.addAll(CommitConfig.instance.getUnsupervisedFiles());
-
-        VcsUtils.deleteFiles(
-                files.stream()
-                        .filter(f -> !CommitConfig.instance.isSupervised(f))
-                        .collect(Collectors.toList()),
-                GlobalConfig.projectDir()
-        );
-
+        VcsUtils.deleteFiles(CommitConfig.instance.getUnsupervisedFiles(), GlobalConfig.projectDir());
     }
 }
